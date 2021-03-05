36a4854ec4a1c85fe33ba6e6fe2bbc4f0973c39e
brynrmrzz <50176541+brynrmrzz@users.noreply.github.com>
2021-03-05 15:51:20 -0500

    Improvements
    Just added some improvements to functionality.

--- python_scripts/simple_calc_w_tkinter.py
+++ python_scripts/simple_calc_w_tkinter.py
@@ -1,6 +1,6 @@
 ## The purpose of this was really to follow along and get a general introduction with Tkinter as I had never used it before, 
 ## The calculator's functionality is laughable but overall I was able to abstract the functions from it that I needed
-## With what I gathered from this I'm a bit closer to manifesting some other ideas that I have. 
+## With what I gathered from this I'm a bit closer to being able to put together some other ideas that I have. 
 
 #Import from tkinter everything
 from tkinter import *
@@ -10,13 +10,13 @@ root = Tk()
 
 #Here we are call our main widget it and then apply the title
 root.title('Calculator')
-#we then apply a size for our window, this is what worked best for me
-root.geometry('232x294')
+#we can then just call our geometry object to make our border. I wrapped it in int() because the float result was causing an error. 
+root.geometry('250x300')
 # and then we added some color for fun 
-root.config(bg='red')
+root.config(bg='green')
 
 #This is creating our input widget(or text box)
-entered = Entry(root, width = 35, borderwidth = 5)
+entered = Entry(root, width = 29, borderwidth = 4)
 
 #Defining our click function 
 def button_click(string):    
@@ -84,8 +84,15 @@ def button_divide():
     entered.delete(0, END)
 
 def button_equal():
-    #create a new value with our current number
-    second_num = float(entered.get())
+
+    #check if length is less than 1 
+    if len(entered.get()) < 1 or len(str(first_num)) < 1:
+        entered.delete(0,END)
+        #if the length is less than 1 make it 0 because that means it's undefined
+        entered.insert(0, first_num)
+        return
+    else:
+        second_num = float(entered.get())
     #then delete the value so it is not displayed alongside new values
     entered.delete(0, END)
     # check our operator for which method to apply
@@ -112,6 +119,10 @@ def button_equal():
         total = first_num / second_num
         #display our total
         entered.insert(0, str(total))
+    #here we have a our final else case meaning operator was never defined 
+    else:
+        entered.insert(0, int(first_num))
+        
 
 def button_clear():
     #Clearing our text box entirely for new input
@@ -119,28 +130,28 @@ def button_clear():
     
 #Here is the creation and visualization of our buttons widgets
 button_1 = Button(root, text = '1', padx = 30, pady = 10, command = lambda : button_click(1))
-button_2 = Button(root, text = '2', padx = 31, pady = 10, command = lambda : button_click(2))
-button_3 = Button(root, text = '3', padx = 31, pady = 10, command = lambda : button_click(3))
+button_2 = Button(root, text = '2', padx = 30, pady = 10, command = lambda : button_click(2))
+button_3 = Button(root, text = '3', padx = 30, pady = 10, command = lambda : button_click(3))
 #This white space helps me group them logically for legibility 
 button_4 = Button(root, text = '4', padx = 30, pady = 10, command = lambda : button_click(4))
-button_5 = Button(root, text = '5', padx = 31, pady = 10, command = lambda : button_click(5))
-button_6 = Button(root, text = '6', padx = 31, pady = 10, command = lambda : button_click(6))
+button_5 = Button(root, text = '5', padx = 30, pady = 10, command = lambda : button_click(5))
+button_6 = Button(root, text = '6', padx = 30, pady = 10, command = lambda : button_click(6))
 #We call the number in a lambda function so that we may enter a integer with our function
 button_7 = Button(root, text = '7', padx = 30, pady = 10, command = lambda : button_click(7))
 button_8 = Button(root, text = '8', padx = 30, pady = 10, command = lambda : button_click(8))
 button_9 = Button(root, text = '9', padx = 30, pady = 10, command = lambda : button_click(9))
 #These functions were added in to test my understanding of the material 
-button_negative = Button(root, text = '-/+', padx = 24, pady = 10, command = convert_negative)
-button_0 = Button(root, text = '0', padx = 31, pady = 10, command = lambda : button_click(0))
-button_dec = Button(root, text = '.', padx = 32, pady = 10, command = lambda : button_click('.'))
+button_negative = Button(root, text = '-/+', padx = 25, pady = 10, command = convert_negative)
+button_0 = Button(root, text = '0', padx = 30, pady = 10, command = lambda : button_click(0))
+button_dec = Button(root, text = ' . ', padx = 30, pady = 10, command = lambda : button_click('.'))
 #these were included in the tutorial but I edited their appearance to fit with my new buttons
-button_sub = Button(root, text = '-' , padx = 30, pady = 10, command = button_sub)
-button_multi = Button(root, text = '*' , padx = 31, pady = 10, command = button_multi)
-button_divide = Button(root, text = '\ ' , padx = 31, pady = 10, command = button_divide)
+button_sub = Button(root, text = ' -' , padx = 30, pady = 10, command = button_sub)
+button_multi = Button(root, text = '*' , padx = 30, pady = 10, command = button_multi)
+button_divide = Button(root, text = '\ ' , padx = 30, pady = 10, command = button_divide)
 #Again we initialize our buttons assigning them visual properties and defining their function
 button_plus = Button(root, text = '+', padx = 30, pady = 10, command = button_add)
 button_equal = Button(root, text = '=', padx = 30, pady = 10, command = button_equal)
-button_clear = Button(root, text = 'Clear', padx = 19, pady = 10, command = button_clear)
+button_clear = Button(root, text = 'Clear', padx = 20, pady = 10, command = button_clear)
 
 # Here we begin actually displaying our graphics first with our text box
 entered.grid(row = 0, column = 0, padx = 1, pady = 1, columnspan = 3)
